54913a9ef41d065d79938c12916f4d41a079155f
dudnikovoleg <dudnikovoleg92@gmail.com>
2016-10-24 23:12:13 +0300

    version 1.2

--- script.js
+++ script.js
@@ -5,23 +5,30 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider,$
 
     $urlRouterProvider.otherwise('/home');
     $stateProvider
+//--------------link for Home
         .state('home', {
-        url: '/home',
-        templateUrl: 'index.html'
-
+                    url: '/home',
+                    templateUrl: 'index.html'
     })
-
-    .state('prod', {
-        url: '/prod',
-        templateUrl: 'product.html'
-
+//-------------link for categori1
+        .state('categori1', {
+        url: '/categori1',
+        templateUrl: 'categories/categori1.html'
     })
+        .state('categori2', {
+            url: '/categori2',
+            templateUrl: 'categories/categori2.html'
+        })
+        .state('categori3', {
+            url: '/categori3',
+            templateUrl: 'categories/categori3.html'
+        })
 }]);//---------------------end router
 //-------------------------star myCtrl
     myApp.controller("myCtrl",['$scope','$http', function ($scope, $http) {
 
 
-        $http.get('tsconfig.json ')
+        $http.get('products.json ')
             .success(function (result) {
                 console.log(result);
                 $scope.items = result;
@@ -32,8 +39,8 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider,$
         });
         //------end $http get
         //-------start function who mast be get id
-        $scope.somF = function  (value) {
-            productFontImg = value;
+        $scope.somF = function  (id) {
+            productFontImg = id;
             console.log(productFontImg)
         };
 
